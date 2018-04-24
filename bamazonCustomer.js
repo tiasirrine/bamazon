8aa616bda75d6df5df798f4cd5a95aa061f6bb63
Tia Sirrine <tiasirrine@outlook.com>
2018-04-24 08:53:25 -0700

    customer prompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,10 +17,57 @@ connection.connect(function(err) {
 
 //this will display all items avaialable upon connection
 function displayItems() {
-  connection.query("SELECT * FROM products", function(err, res) {
+  connection.query("SELECT * FROM products", function(err, results) {
     if (err) throw err;
-    console.log(res);
+    console.log(results);
+    customerPrompt();
   });
 }
 
-//
+//prompt module that will walk customer through selecting an item
+
+function customerPrompt() {
+  inquirer
+    .prompt([
+      {
+        name: "itemID",
+        type: "input",
+        message: "What is the item ID you would like to buy?"
+      },
+      {
+        name: "quantity",
+        type: "input",
+        message: "How many units would you like to buy?"
+      }
+    ])
+    .then(function(answer) {
+      console.log(answer);
+      var itemID = answer.itemID;
+      var units = answer.quantity;
+      connection.query(
+        "SELECT * FROM products WHERE id = ?",
+        [itemID],
+        function(err, results) {
+          if (err) throw err;
+          if (results.length === 0) {
+            console.log("Unavailable");
+            customerPrompt();
+          } else if (results[0].stock_quantity >= units) {
+            //console.log("here");
+            connection.query(
+              "UPDATE products SET stock_quantity = ? WHERE id = ?;",
+              [results[0].stock_quantity - units, itemID],
+              function(err, res) {
+                if (err) throw err;
+                console.log("Your order is on it's way!");
+                customerPrompt();
+              }
+            );
+          } else if (units > results[0].stock_quantity) {
+            console.log("Sorry, Not Enough!");
+            customerPrompt();
+          }
+        }
+      );
+    });
+}
